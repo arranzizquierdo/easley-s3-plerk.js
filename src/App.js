a1571d4b10d91aebfe8d8145cef45a3d14d1317c
katia1802 <katia.rojas.sandoval@gmail.com>
2019-01-08 21:06:01 +0100

    Index HTMTL y scss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+        <section class="container__cardview">
+        <div className="container__buttom">
+      <button class="buttom__reset" type="button"><i class="icon__trash far fa-trash-alt icon__trash"></i>Reset</button>
+    </div>
+
+  <div className="preview__card">
+    <div className="rectangle__decoration"></div>
+    <div className="name__container">
+      <p className="name">Nombre Apellido</p>
+      <p className="profession">Front-end developer</p>
+    </div>
+
+    <img className="photo__user" src="assets/images/default.jpeg" alt="User photo" />
+    <ul className="social__media-list">
+      
+      <li className="social__media-buttom">
+        <a className="icon__number icon__item" href="">
+          <i className="fas fa-mobile-alt">
+          </i></a>
+      </li>
+      
+      <li className="social__media-buttom">
+        <a className="icon__mail icon__item" href="">
+          <i className="far fa-envelope">
+          </i></a>
+      </li>
+      
+      <li className="social__media-buttom">
+        <a className="icon__linkedin icon__item" href="">
+          <i className="fab fa-linkedin-in">
+          </i></a>
+      </li>
+      
+      <li className="social__media-buttom">
+        <a className="icon__github icon__item" href="">
+          <i className="fab fa-github-alt">
+          </i></a>
+      </li>
+    </ul>
+
+    <div className="rectangle__decoration-large"></div>
+    <ul className="skill__tags">
+      <li className="first_tag tag">HTML</li>
+      <li className="second_tag tag">CSS</li>
+      <li className="third_tag tag">GULP</li>
+    </ul>
+    </div>
+  </section>
+  </div>
+
     );
   }
 }
