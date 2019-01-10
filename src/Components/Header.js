03fc4bd63e204127209e007973e20386175216ff
arranzizquierdo <laura.arranz88@gmail.com>
2019-01-10 20:30:26 +0100

    Nothing changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import LogoAwesome from '../Images/logo-awesome.svg';
-import './scss/layout/_header.scss';
-
 
 class Header extends Component {
     render () {
         return (
-            <header className="@@header">
+            <header className="header__card-creator">
                 <a className="@@link-to-index" href="https://adalab.es/">
-                <img className="@@logo__awesome" src={LogoAwesome} alt="Logo Awesome Profile Cards" />
+                <img className="logo__card-creator" src={LogoAwesome} alt="Logo Awesome Profile Cards" />
                 </a>
             </header>
         );
