10c342efc915c7cda1ae63fca5a6365cf941ed78
arranzizquierdo <laura.arranz88@gmail.com>
2019-01-11 09:45:34 +0100

    Create social link component

--- src/Components/CardPreview.js
+++ src/Components/CardPreview.js
@@ -1,61 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ImageDefault from '../Images/default.jpeg';
+import SocialLink from './SocialLink';
 
 class CardPreview extends Component {
-    render () {
-        return (
+  render() {
+    return (
 
-            <section className="container__cardview">
-            <div className="container__buttom">
+      <section className="container__cardview">
+        <div className="container__buttom">
           <button className="buttom__reset" type="button"><i className="icon__trash far fa-trash-alt icon__trash"></i>Reset</button>
         </div>
-    
-      <div className="preview__card">
-        <div className="rectangle__decoration"></div>
-        <div className="name__container">
-          <p className="name">Nombre Apellido</p>
-          <p className="profession">Front-end developer</p>
-        </div>
-    
-        <img className="photo__user" src={ImageDefault} alt="User photo" />
-        <ul className="social__media-list">
-          
+
+        <div className="preview__card">
+          <div className="rectangle__decoration"></div>
+          <div className="name__container">
+            <p className="name">Nombre Apellido</p>
+            <p className="profession">Front-end developer</p>
+          </div>
+
+          <img className="photo__user" src={ImageDefault} alt="User photo" />
+
+          <ul className="social__media-list">
           <li className="social__media-buttom">
-            <a className="icon__number icon__item" href="">
-              <i className="fas fa-mobile-alt">
-              </i></a>
-          </li>
-          
-          <li className="social__media-buttom">
-            <a className="icon__mail icon__item" href="">
-              <i className="far fa-envelope">
-              </i></a>
-          </li>
-          
-          <li className="social__media-buttom">
-            <a className="icon__linkedin icon__item" href="">
-              <i className="fab fa-linkedin-in">
-              </i></a>
-          </li>
-          
-          <li className="social__media-buttom">
-            <a className="icon__github icon__item" href="">
-              <i className="fab fa-github-alt">
-              </i></a>
-          </li>
-        </ul>
-    
-        <div className="rectangle__decoration-large"></div>
-        <ul className="skill__tags">
-          <li className="first_tag tag">HTML</li>
-          <li className="second_tag tag">CSS</li>
-          <li className="third_tag tag">GULP</li>
-        </ul>
+              <SocialLink 
+                itemIcon="fas fa-mobile-alt"
+              />
+            </li>
+            <li className="social__media-buttom">
+              <SocialLink 
+                itemIcon="far fa-envelope"
+              />
+            </li>
+            <li className="social__media-buttom">
+              <SocialLink 
+                itemIcon="fab fa-linkedin-in"
+              />
+            </li>
+            <li className="social__media-buttom">
+              <SocialLink 
+                itemIcon="fab fa-github-alt"
+              />
+            </li>
+          </ul>
+
+          <div className="rectangle__decoration-large"></div>
+          <ul className="skill__tags">
+            <li className="first_tag tag">HTML</li>
+            <li className="second_tag tag">CSS</li>
+            <li className="third_tag tag">GULP</li>
+          </ul>
         </div>
       </section>
 
-        )
-    }
+    )
+  }
 };
 
 export default CardPreview;
