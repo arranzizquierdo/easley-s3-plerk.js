a4776375dd78b63cda837207a4c0fb467c32bf20
elisamartinb <elisamartinbt@gmail.com>
2019-01-14 19:05:25 +0100

    fix merge collapsibles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import CollapsiblesThree from './Components/collapsibles3';
-=======
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import CardPreview from './Components/CardPreview';
->>>>>>> 191ccab581702674f1347f64de9f07335180bceb
 
 class App extends Component {
   render() {
