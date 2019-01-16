408df215a5a6c7d5b3f1f81b7f3df899a67f9b62
rdfornieles <rdfornieles@gmail.com>
2019-01-16 11:57:03 +0100

    close branch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import CardPreview from './Components/CardPreview';
 
+
 class App extends Component {
   constructor (props){
     super (props);
