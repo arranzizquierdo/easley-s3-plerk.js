021529f10676f5f7f37eac7f3d2b4ecf618e9622
Paloma Ania <paloma.aniabenito@gmail.com>
2019-01-25 13:26:31 +0100

    Updates

---
+++ precache-manifest.8b6781ec37ddc429408dc1cae41d27aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "/easley-s3-plerk.js/static/media/logo-awesome.0118e311.svg"
+  },
+  {
+    "revision": "a8a9d1fcbfc33336ad26",
+    "url": "/easley-s3-plerk.js/static/js/runtime~main.a8a9d1fc.js"
+  },
+  {
+    "revision": "35fe114270b1ba189ed9",
+    "url": "/easley-s3-plerk.js/static/js/main.35fe1142.chunk.js"
+  },
+  {
+    "revision": "fe856dacbf6212a62ed5",
+    "url": "/easley-s3-plerk.js/static/js/1.fe856dac.chunk.js"
+  },
+  {
+    "revision": "35fe114270b1ba189ed9",
+    "url": "/easley-s3-plerk.js/static/css/main.04e892a4.chunk.css"
+  },
+  {
+    "revision": "1aed4aed40e79031fdc0df1e2998b5e7",
+    "url": "/easley-s3-plerk.js/index.html"
+  }
+];
