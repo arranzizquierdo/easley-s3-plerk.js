cb68a87fa0da162ca591a96ec13f82b8a77754a9
arranzizquierdo <laura.arranz88@gmail.com>
2019-01-29 19:22:54 +0100

    Style mobile responsive

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,12 +9,15 @@ class Footer extends Component {
           <div className="copyright__container">
             <small className="copyright">Awesome profile-cards &copy2018</small>
           </div>
-          <a href="https://github.com/Adalab/easley-s3-plerk.js" className="adalab-logo__link">
-            <img src={logoPlerk} alt="Logo Plerk" className="adalab-logo__image" />
+          <div className="container__logos">
+          <a href="https://github.com/Adalab/easley-s3-plerk.js" className="adalab-logo__link plerk-link">
+            <img src={logoPlerk} alt="Logo Plerk" className="adalab-logo__image plerk-logo" />
           </a>
           <a href="https://adalab.es/" className="adalab-logo__link">
             <img src={logoAdalab} alt="Logo de Adalab" className="adalab-logo__image" />
           </a>
+          </div>
+          
       </footer>
     )
   }
