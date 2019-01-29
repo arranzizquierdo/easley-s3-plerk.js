1c5e40bc524a3148dc9e12fe62f02d3d059cc5a6
arranzizquierdo <laura.arranz88@gmail.com>
2019-01-29 19:15:43 +0100

    Add plerk logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import logoAdalab from '../Images/logo-adalab.png';
+import logoPlerk from '../Images/plerkjs-logo.png';
 
 class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <div className="footer-container">
           <div className="copyright__container">
             <small className="copyright">Awesome profile-cards &copy2018</small>
-            <img src="assets/images/free_horizontal_on_white_by_logaster.png" alt="" className="logo__image" />
           </div>
-
+          <a href="https://github.com/Adalab/easley-s3-plerk.js" className="adalab-logo__link">
+            <img src={logoPlerk} alt="Logo Plerk" className="adalab-logo__image" />
+          </a>
           <a href="https://adalab.es/" className="adalab-logo__link">
             <img src={logoAdalab} alt="Logo de Adalab" className="adalab-logo__image" />
           </a>
-        </div>
       </footer>
     )
   }
